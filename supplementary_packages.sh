93beaab79316fc62049ad44c4776512438c86c92
Dee Reddy <d.reddy@yandex.com>
2017-06-25 12:15:14 -0400

    Refactor: Separate nVidia drivers.

--- supplementary_packages.sh
+++ supplementary_packages.sh
@@ -62,13 +62,6 @@ sudo apt-get install vlc -yq
 echo "/nInstalling qBittorrent..."
 sudo apt-get install qbittorrent -yq
 
-# Install Nvidia drivers:
-# n.b. the version of your package depends on your OS and card (don't use .run files)
-# see https://help.ubuntu.com/community/BinaryDriverHowto/Nvidia
-echo "/nInstalling Nvidia 375 drivers..."
-echo "/nNote: The specific package you need will depend on your OS and your card."
-sudo apt-get install nvidia-375 nvidia-settings
-
 # Install vim:
 echo "/nInstalling vim.."
 sudo apt-get install vim -yq
