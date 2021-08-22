4e02039d43f9b2b2559e7fdfed32121bd028203b
Dee Reddy <d.reddy@yandex.com>
2021-08-22 16:00:19 -0400

    Refactor: split into interactive and non-interactive scripts; fix issues in interactive installation packages.

---
+++ interactive_packages.sh
@@ -0,0 +1,58 @@
+# ########## install Conda: ##########
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P ~/Downloads/
+sh ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
+
+# TODO: follow the instructions in the Conda interactive installation
+# TODO: after installing minconda, restart your terminal before continuing:
+
+# ensure base environment isn't auto-activated <https://github.com/conda/conda/issues/8211>:
+conda config --set auto_activate_base false && conda deactivate
+
+# create a conda environment called "ml" (machine learning):
+conda create -n ml python=3.8 tensorflow jupyter matplotlib pandas
+
+
+# ########## Docker-post install steps: ##########
+# <https://docs.docker.com/engine/install/linux-postinstall/>
+
+# run Docker without root privileges -- n.b. this can be a security issue in some cases <https://docs.docker.com/engine/security/#docker-daemon-attack-surface>
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+
+# TODO: log out of your session and log back in before continuing with the next steps:
+newgrp docker
+
+# test that Docker was installed properly:
+docker run hello-world
+
+# configure Docker to start on boot:
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+
+# ########## install nvm and NodeJS: ##########
+# TODO: you might want to check if this is latest version of nvm before running script:
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# TODO: after installation is done, restart terminal before continuing:
+nvm install --lts && nvm use --lts
+
+
+
+# ########## misc. ##########
+# N.b. the rest of this script includes optional/alternatives packages that may be useful in certain cases
+
+# ########## (OPTIONAL) download TensorFlow Docker image: ##########
+# The Docker version of TensorFlow enables GPU. On the other hand, the "normal" conda installation runs TensorFlow on CPU.
+docker pull tensorflow/tensorflow:latest-py3-jupyter
+# create aliases:
+echo "\nalias docker_tensorflow=\"docker run -u \$(id -u):\$(id -g) -it -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter\"" >> ~/.bash_aliases
+
+# ########## (OPTIONAL) install pip and pipenv: ##########
+
+sudo apt install python3-pip -yq
+pip install --user pipenv
+
+# reinstall due to this issue: <https://stackoverflow.com/q/51225750>
+sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
--- ubuntu_local_setup.sh
+++ ubuntu_local_setup.sh
@@ -1,6 +1,8 @@
+!/bin/bash
+
 sudo apt update
 
-# ----- install useful packages: -----
+# ########## install useful packages: ##########
 sudo apt install git -yq
 sudo apt install neovim -yq
 sudo apt install curl -yq
@@ -8,22 +10,18 @@ sudo apt install neofetch -yq
 sudo apt install kompare -yq
 sudo snap install keepassxc -yq
 
-# ----- install nvm and NodeJS: -----
-# TODO: check if this is latest version of nvm before running script:
-# TODO: this might not work without closing terminal and restarting:
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm" && \. "$NVM_DIR/nvm.sh"
-
-nvm install --lts && nvm use --lts
-
-# ----- install apps: -----
+# ########## install apps: ##########
 sudo apt install chromium-browser -yq
 
-# ----- install Jetbrains IDEs: -----
+# ########## install Jetbrains IDEs: ##########
 sudo snap install pycharm-professional --classic -yq
 sudo snap install webstorm --classic -yq
 
-# ----- install Docker: -----
+# ########## misc. ##########
+# create bash aliases file:
+touch ~/.bash_aliases
+
+# ########## install Docker: ##########
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -40,49 +38,5 @@ echo \
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -yq
 
-# ----- Docker-post install steps: -----
-# <https://docs.docker.com/engine/install/linux-postinstall/>
-
-# run Docker without root privileges -- n.b. this can be a security issue in some cases <https://docs.docker.com/engine/security/#docker-daemon-attack-surface>
-sudo groupadd docker
-sudo usermod -aG docker $(whoami)
-exec bash
-exit
-
-# TODO: you may need to log out of your session and log back in before running this:
-newgrp docker
-
-echo "\nTesting that Docker was installed properly:\n"
-docker run hello-world
-
-# configure Docker to start on boot:
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
-# ----- install conda: -----
-# download and install in one line <https://serverfault.com/a/226391>:
-wget -O - https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | bash
-exec bash
-exit
-
-# after conda has been installed, ensure base environment isn't auto-activated <https://github.com/conda/conda/issues/8211>:
-conda config --set auto_activate_base false
-
-# create conda environment:
-conda create -n nn python=3.8 tensorflow jupyter matplotlib
-
-# ----- misc. -----
-# these are commented out because they may not be necessary, but are good to have for reference
-
-## Download TensorFlow Docker image:
-# docker pull tensorflow/tensorflow:latest-py3-jupyter
-# Create aliases:
-# echo "\nalias docker_tensorflow=\"docker run -u \$(id -u):\$(id -g) -it -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter\"" >> ~/.bash_aliases
-# cat ~/.bash_aliases
-
-## pip3 and pipenv:
-# sudo apt install python3-pip -yq
-# pip install --user pipenv
-# 
-## reinstall due to this issue: <https://stackoverflow.com/q/51225750>
-# sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
+echo "\n\nIMPORTANT: Remember to run the manual-install-script after installation!\n\n"
+sleep 2
