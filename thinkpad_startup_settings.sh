6123355c6b603f2c82c932ff90965f91d7985570
Dee Reddy <d.reddy@yandex.com>
2021-08-23 05:48:04 +0300

    Refactor: Remove obsolute thinkpoint trackpoint config script.

--- thinkpad_startup_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Add this script to your distro's system startup -- in KDE, this can be done in the program "Autostart".
-
-
-# Fixes ThinkPad's TrackPoint sensitivity:
-# n.b. it looks for the device name "TPPS/2 IBM TrackPoint":
-TRACKPOINTID="$(xinput list --id-only 'TPPS/2 IBM TrackPoint')"
-xinput --set-prop "${TRACKPOINTID}" "libinput Accel Speed" -0.7
