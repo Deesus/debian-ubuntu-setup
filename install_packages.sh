a8d2c0c21c6b490d7521aeb5794773b7183af8a9
Dee Reddy <deesus@pm.me>
2022-06-03 08:01:27 -0700

    Refactor: add docker-compose.

--- install_packages.sh
+++ install_packages.sh
@@ -61,7 +61,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -yq
+sudo apt-get install docker-ce docker-compose docker-ce-cli containerd.io -yq
 
 echo "\n\nIMPORTANT: Remember to run the manual-install-script after installation!\n\n"
 sleep 2
